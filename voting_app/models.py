e731aa2a7af0e5580f9493a2d4c17b0e623d943f
AbdurrohmanJCom <tuckerticket@gmail.com>
2023-12-05 15:56:30 +0500

    voting_app: models changed

--- voting_app/models.py
+++ voting_app/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Voter(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add other voter-specific fields
+class User(models.Model):
+    firstname = models.CharField(max_length=15)
+    secondname = models.CharField(max_length=15)
+    thirdname = models.CharField(max_length=15, blank=True, null=True)
+    birth_date = models.DateField()                                   
+    date_created = models.DateTimeField(auto_not_add=True)
+    identification = models.PositiveIntegerField()
 
