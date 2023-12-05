2d16d3616ac01e114c285e4e4d483981671e87d4
AbdurrohmanJCom <tuckerticket@gmail.com>
2023-12-05 16:33:51 +0500

    vote added

--- gov_app/models.py
+++ gov_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from voting_app.models import User
 
 
 class PoliticalParty(models.Model):
@@ -7,12 +7,14 @@ class PoliticalParty(models.Model):
     logo = models.ImageField(upload_to='', null=True, blank=True)
     leader = models.OneToOneField('gov_app.Candidate', on_delete=models.SET_NULL, null=True, blank=True)
     ideology = models.TextField()
-    year_founded = models.PositiveIntegerField()
+    year_founded = models.DateField()
     slogan = models.CharField(max_length=255)
     social_media_links = models.URLField(max_length=255, blank=True, null=True)
     contact_information = models.TextField()
 
-
+    def __str__(self):
+        return self.title
+        
 class Candidate(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     avatar = models.ImageField(upload_to='', null=True, blank=True)
@@ -21,3 +23,5 @@ class Candidate(models.Model):
     contact_information = models.TextField()
     social_media_links = models.CharField(max_length=255, blank=True, null=True)
 
+    def __str__(self):
+        return f'{self.user.firstname} {self.user.secondname} | {self.social_media_links}'
--- voting_app/models.py
+++ voting_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from gov_app.models import *
 
 class User(models.Model):
     firstname = models.CharField(max_length=15)
@@ -9,3 +9,14 @@ class User(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     identification = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'{self.firstname} {self.secondname}'
+
+
+class Vote(models.Model):
+    user = models.OneToOneField(User)
+    voted_candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    voted_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.firstname} {self.user.secondname} {self.voted_candidate}'
