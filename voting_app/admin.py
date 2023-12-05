eef437422cab77ea80f9324994734d22a7d3470e
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-12-05 14:45:30 +0500

    apps added to settings

--- voting_app/admin.py
+++ voting_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Voter, Candidate
 
-# Register your models here.
+admin.site.register(Voter)
+admin.site.register(Candidate)
--- voting_system/settings.py
+++ voting_system/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'voting_app',
+    'gov_app',
+    'widget-tweaks',
 ]
 
 MIDDLEWARE = [
