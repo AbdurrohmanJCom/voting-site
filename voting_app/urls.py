f5b1edf889eff2cdb3b5b252c46ff8536922c728
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-12-16 17:22:30 +0500

    login logout added

--- voting_app/urls.py
+++ voting_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('login/', views.login_views, name='login'),
-    path('logout/', views.logout_views, name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
 ]
